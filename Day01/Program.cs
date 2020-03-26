08eb32dc04fc069f50fe38c8672e14868b5675ac
Bilal Taherkheli <bilal76@gmail.com>
2020-03-26 21:25:59 +0100

    D1a done despite being too drunk ;)

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
 
 namespace Day01
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
-      Console.WriteLine("Hello World!");
+      string path = "input.txt";
+      Console.WriteLine("\n Instructions took Santa to floor: {0}", GetFloor(LoadInput(path)));
+    }
+
+    private static int GetFloor(string input)    
+    {
+      int floor = 0;
+      var directions = input.ToCharArray();
+
+      foreach (var direction in directions)
+      {
+        if ((direction != '(') && (direction != ')'))
+          throw new ArgumentException("Bad input. only ( or ) is valid!");
+        
+        if (direction == '(')
+          floor += 1;
+        else
+          floor -= 1;
+      }
+
+      return floor;
+    }
+
+    private static string LoadInput(string path)
+    {
+      return new StreamReader(path).ReadToEnd();
     }
   }
 }
