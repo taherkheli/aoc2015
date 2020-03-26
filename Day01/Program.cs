51c976f1d57b95cdf8a84cbddbcd15b36b387c32
Bilal Taherkheli <bilal76@gmail.com>
2020-03-26 21:00:46 +0100

    initial commit

---
+++ Day01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# AdventOfCode2015
+A repo to play with AdventOfCode 2015 because 20+19 got too hard to handle ;) at Day 14
+
+https://adventofcode.com/
+
