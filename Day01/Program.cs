daecd265c7baf191e9c94c4c4314748743920e4f
Bilal Taherkheli <bilal76@gmail.com>
2020-03-26 21:48:20 +0100

    wow! 2015 seems to be at the right level for my programming ability. I did it despite having had loads of wine ;)

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -8,7 +8,9 @@ namespace Day01
     static void Main()
     {
       string path = "input.txt";
-      Console.WriteLine("\n Instructions took Santa to floor: {0}", GetFloor(LoadInput(path)));
+      Console.WriteLine("\nPart I:  Instructions took Santa to floor: {0}", GetFloor(LoadInput(path)));
+
+      Console.WriteLine("\nPart II:  Position of the chracter that made Santa enter the floor: {0}", IndexAtEnteringBasement(LoadInput(path)));
     }
 
     private static int GetFloor(string input)    
@@ -16,18 +18,47 @@ namespace Day01
       int floor = 0;
       var directions = input.ToCharArray();
 
-      foreach (var direction in directions)
+      foreach (var dir in directions)
       {
-        if ((direction != '(') && (direction != ')'))
+        if ((dir != '(') && (dir != ')'))
           throw new ArgumentException("Bad input. only ( or ) is valid!");
         
-        if (direction == '(')
+        if (dir == '(')
           floor += 1;
         else
           floor -= 1;
       }
 
       return floor;
+    }
+
+    private static int IndexAtEnteringBasement(string input)
+    {
+      int result = 0;
+      int floor = 0;
+      var directions = input.ToCharArray();
+
+      for (int i = 0; i < input.Length; i++)
+      {
+        if ((input[i] != '(') && (input[i] != ')'))
+          throw new ArgumentException("Bad input. only ( or ) is valid!");
+
+        if (input[i] == '(')
+          floor += 1;        
+        else
+        {
+          floor -= 1;
+
+          if (floor < 0)
+          {
+            result = i + 1;
+            break;
+          }
+        }
+
+      }
+
+      return result;
     }
 
     private static string LoadInput(string path)
