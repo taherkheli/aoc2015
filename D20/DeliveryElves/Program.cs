df3be1d6a678e87bcd67dc335467d1168f2cf3fc
Bilal Taherkheli <bilal76@gmail.com>
2020-04-14 02:16:15 +0200

    Started out with some fancy classes for Elf and House but soon realized that it is not gonna cut it. Cut it down to bare bones. If you start at the right number this is lightening fast ;)

---
+++ D20/DeliveryElves/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DeliveryElves
+{
+  class Program
+  {
+    static void Main()
+    {
+      int target = 36000000;
+      int n = 831500; // to not loop forever :)
+
+      while (true)
+      {
+        if (HasReachedTarget(n, target))
+          break;
+
+        n++;
+      }
+
+      Console.WriteLine("\nPartI: The lowest house number of the house to get at least 36000000 presents is: {0}", n);
+    }
+
+    //Given a number(id) of a house, calculate presents received. Return true if amount exceeds target otherwise 
+    private static bool HasReachedTarget(int num, int target)
+    {
+      int presents = 0;
+
+      for (int i = 1; i < num + 1; i++)
+      {
+        var temp = 0;
+
+        if (num % i == 0)   //will it deliver to me?
+          temp = 10 * i;    //how much?
+        
+        //Console.WriteLine("elf [{0}] delivered = {1}", i, temp);
+
+        presents += temp;
+      }
+
+      //Console.WriteLine("TOTAL = {0}\n", presents);
+
+      if (presents >= target)
+        return true;
+      else 
+        return false;
+    }
+  }
+}
