ec09a46ca328d475ceb062fc51a5c6d5f90b4329
Bilal Taherkheli <bilal76@gmail.com>
2020-04-15 19:41:57 +0200

    D15a done. depressing as I had to look it up for the first time in all of 2015 so far :(. Could not get the loop to work and it was super simple in the end

--- D15/CookieMaking/Program.cs
+++ D15/CookieMaking/Program.cs
@@ -6,6 +6,32 @@ namespace CookieMaking
   {
     static void Main()
     {
+      const int total = 100;
+      var path = "input.txt";
+      var ingredients = Utils.Parse(path);
+      var cookie = new Cookie(ingredients);
+
+      var maxScore = int.MinValue;
+
+      for (int a = 0; a < total; a++)
+      {
+        for (int b = 0; b < total - a; b++)
+        {
+          for (int c = 0; c < total - a - b; c++)
+          {
+            var d = total - a - b - c;
+            //Console.WriteLine("a:[{0}]   b:[{1}]   c:[{2}]   d:[{3}]", a, b, c, d);
+            var cfg = new int[4] { a, b, c, d };
+            cookie.Cfg = cfg;
+            var score = cookie.Calculate();
+
+            if (score > maxScore)
+              maxScore = score;
+          }
+        }
+      }
+
+      Console.WriteLine("\nPartI: The total score of the highest-scoring cookie: {0}", maxScore);
     }
   }
 }
--- D15/CookieMaking/Utils.cs
+++ D15/CookieMaking/Utils.cs
@@ -1,9 +1,34 @@
-﻿namespace CookieMaking
+﻿using System;
+using System.IO;
+
+namespace CookieMaking
 {
   public static class Utils
   {
-    public static void Parse(string path)
+    public static Ingredient[] Parse(string path)
     {
+      var file = new StreamReader(path);
+      var lines = file.ReadToEnd().Trim().Trim(Environment.NewLine.ToCharArray()).Split(Environment.NewLine);
+      var result = new Ingredient[lines.Length];
+
+      for (int i = 0; i < result.Length; i++)      
+        result[i] = Create(lines[i]);
+
+      return result;
+    }
+
+    private static Ingredient Create(string line)
+    {
+      var result = new Ingredient();      
+      var s = line.Trim().Split(' ');
+      
+      result.Capacity = int.Parse(s[2].Trim().Trim(','));
+      result.Durability = int.Parse(s[4].Trim().Trim(','));
+      result.Flavor = int.Parse(s[6].Trim().Trim(','));
+      result.Texture = int.Parse(s[8].Trim().Trim(','));
+      result.Calories = int.Parse(s[10].Trim());
+
+      return result;
     }
   }
 }
