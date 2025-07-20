4397baaa86dc6e1d30515b464de80e386e3bd6e2
Bilal Taherkheli <bilal76@gmail.com>
2025-07-20 13:11:26 +0200

    - Day 3 completed

---
+++ src/D03/Dir.cs
@@ -0,0 +1,11 @@
+﻿namespace aoc.D03
+{
+	public enum Dir
+  {
+    North,
+    South,
+    East,
+    West,
+    Unknown
+  }
+}
---
+++ src/D03/Parser.cs
@@ -0,0 +1,31 @@
+﻿namespace aoc.D03
+{
+  public static class Parser
+  {
+    public static Dir[] Parse(string path)
+    {
+      Dir[] result;
+      var text = File.ReadAllLines(path)[0].Trim();
+      result = new Dir[text.Length];
+
+      for (int i = 0; i < result.Length; i++)
+        result[i] = GetDirection(text[i]);
+
+      return result;
+    }
+
+    private static Dir GetDirection(char s)
+    {
+      var result = s switch
+      {
+        '<' => Dir.West,
+        '>' => Dir.East,
+        '^' => Dir.North,
+        'v' => Dir.South,
+        _ => Dir.Unknown,
+      };
+
+      return result;
+    }
+  }
+}
