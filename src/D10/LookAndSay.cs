e28864075f64adf87fc12c3f39c1e4cab49cff3f
Bilal Taherkheli <bilal76@gmail.com>
2025-07-30 18:15:00 +0200

    Day 10 completed

---
+++ src/D10/LookAndSay.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace aoc.D10
+{
+  public static class LookAndSay 
+  {
+
+    public static int Solve(string s, int loopCount)
+    {
+      string result = s;
+      
+      for (int i =0; i < loopCount; i++) 
+      {
+        int j = 0;
+        var sb = new StringBuilder();
+
+        while(j < result.Length)
+        {
+          var occurences = CountSuccessiveOccurences(result, j);
+          sb.Append(occurences);
+          sb.Append(result[j]);
+          j += occurences;
+        }
+
+        result = sb.ToString();
+      }
+
+      return result.Length; 
+    }
+
+    // count successive occurences of the charcater located at index
+    private static int CountSuccessiveOccurences(string s, int index)
+    {
+      var character = s[index];
+      int count = 1;
+
+      for (int i = index + 1; i < s.Length; i++)
+      {
+        if (character == s[i])
+          count++;
+        else
+          break;
+      }
+
+      return count;
+    }
+  }
+}
