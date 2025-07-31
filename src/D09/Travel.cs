d4b5b8d2ff2227f246dce11e8e915f572de02e10
Bilal Taherkheli <bilal76@gmail.com>
2025-07-31 13:31:30 +0200

    - Day 13 completed

--- src/D09/Travel.cs
+++ src/D09/Travel.cs
@@ -1,4 +1,6 @@
-﻿namespace aoc.D09
+﻿using aoc.Helpers;
+
+namespace aoc.D09
 {
   public class Travel((List<string>, Dictionary<(string, string), int>) input)
   {
@@ -7,35 +9,13 @@
 
     public int GetDistance(bool isPartII = false)
     {
-      var routes = GetPermutations(_cities);
+      var routes = Utils.GetPermutations(_cities);
       var routeDistances = new int[routes.Count];
 
       for (int i = 0; i < routes.Count; i++)
         routeDistances[i] = GetRouteDistance(routes[i]);
 
-      return isPartII ? GetLargest(routeDistances) : GetSmallest(routeDistances);
-    }
-
-    private static int GetSmallest(int[] nums)
-    {
-      var smallest = nums[0];
-
-      for (int i = 1; i < nums.Length; i++)
-        if (nums[i] < smallest)
-          smallest = nums[i];
-
-      return smallest;
-    }
-
-    private static int GetLargest(int[] nums)
-    {
-      var largest = nums[0];
-
-      for (int i = 1; i < nums.Length; i++)
-        if (nums[i] > largest)
-          largest = nums[i];
-
-      return largest;
+      return isPartII ? Utils.GetLargest(routeDistances) : Utils.GetSmallest(routeDistances);
     }
 
     private int GetRouteDistance(List<string> route)
@@ -46,33 +26,6 @@
         sum += _distances[(route[i], route[i + 1])];
 
       return sum; 
-    }
-
-    private static List<List<T>> GetPermutations<T>(List<T> elements)
-    {
-      var result = new List<List<T>>();
-      Permute(elements, 0, elements.Count - 1, result);
-      return result;
-    }
-
-    private static void Permute<T>(List<T> elements, int start, int end, List<List<T>> result)
-    {
-      if (start == end)
-        result.Add([.. elements]);      
-      else
-      {
-        for (int i = start; i <= end; i++)
-        {
-          Swap(elements, start, i);
-          Permute(elements, start + 1, end, result);
-          Swap(elements, start, i); // backtrack
-        }
-      }
-    }
-
-    private static void Swap<T>(List<T> list, int a, int b)
-    {
-      (list[b], list[a]) = (list[a], list[b]);
     }
   }
 }
