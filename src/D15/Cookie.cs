ee65ea975310f9dccdea91d6f30e5c9a7b6e1881
Bilal Taherkheli <bilal76@gmail.com>
2025-08-01 20:52:37 +0200

    - Day 15 completed

---
+++ src/D15/Cookie.cs
@@ -0,0 +1,46 @@
+﻿namespace aoc.D15
+{
+  public class Cookie
+  {
+    private readonly Ingredient[] _ingredients;
+    private int[] _cfg;
+
+    public Cookie(Ingredient[] ingredients)
+    {
+      _ingredients = ingredients;
+      _cfg = new int[4];
+
+      for (int i = 0; i < _cfg.Length; i++)
+        _cfg[i] = 0;
+    }
+
+    public int[] Cfg { get => _cfg; set => _cfg = value; }
+
+    public (int, bool) Calculate()
+    {
+      var sellable = false;
+
+      int calorieCount = _cfg[0] * _ingredients[0].Calories + _cfg[1] * _ingredients[1].Calories + _cfg[2] * _ingredients[2].Calories + _cfg[3] * _ingredients[3].Calories;
+      if (calorieCount == 500)
+        sellable = true; ;
+
+      int capacity = _cfg[0] * _ingredients[0].Capacity + _cfg[1] * _ingredients[1].Capacity + _cfg[2] * _ingredients[2].Capacity + _cfg[3] * _ingredients[3].Capacity;
+      if (capacity < 0)
+        capacity = 0;
+
+      int durability = _cfg[0] * _ingredients[0].Durability + _cfg[1] * _ingredients[1].Durability + _cfg[2] * _ingredients[2].Durability + _cfg[3] * _ingredients[3].Durability;
+      if (durability < 0)
+        durability = 0;
+
+      int flavor = _cfg[0] * _ingredients[0].Flavor + _cfg[1] * _ingredients[1].Flavor + _cfg[2] * _ingredients[2].Flavor + _cfg[3] * _ingredients[3].Flavor;
+      if (flavor < 0)
+        flavor = 0;
+
+      int texture = _cfg[0] * _ingredients[0].Texture + _cfg[1] * _ingredients[1].Texture + _cfg[2] * _ingredients[2].Texture + _cfg[3] * _ingredients[3].Texture;
+      if (texture < 0)
+        texture = 0;
+
+      return (capacity * durability * flavor * texture, sellable);
+    }
+  }
+}
---
+++ src/D15/Ingredient.cs
@@ -0,0 +1,39 @@
+﻿namespace aoc.D15
+{
+  public class Ingredient
+  {
+    private int _capacity;
+    private int _durability;
+    private int _flavor;
+    private int _texture;
+    private int _calories;
+
+    public Ingredient()
+    {
+      _capacity = 0;
+      _durability = 0;
+      _flavor = 0;
+      _texture = 0;
+      _calories = 0;
+    }
+
+    public Ingredient(int capacity,
+                      int durability,
+                      int flavor,
+                      int texture,
+                      int calories)
+    {
+      _capacity = capacity;
+      _durability = durability;
+      _flavor = flavor;
+      _texture = texture;
+      _calories = calories;
+    }
+
+    public int Capacity { get => _capacity; set => _capacity = value; }
+    public int Durability { get => _durability; set => _durability = value; }
+    public int Flavor { get => _flavor; set => _flavor = value; }
+    public int Texture { get => _texture; set => _texture = value; }
+    public int Calories { get => _calories; set => _calories = value; }
+  }
+}
