8b574bd8695bdc795e7f7b194112aa8551f71f44
Bilal Taherkheli <bilal76@gmail.com>
2025-08-03 23:57:55 +0200

    - Day 18 completed

---
+++ src/D18/Parser.cs
@@ -0,0 +1,36 @@
+﻿namespace aoc.D18
+{
+  public static class Parser
+  {
+    public static int[,] Parse(string path)
+    {
+      var lines = File.ReadAllLines(path);
+      int rows = lines.Length;
+      int cols = lines[0].Length;
+      var state = new int[rows, cols];
+
+      for (int r = 0; r < rows; r++)
+        for (int c = 0; c < cols; c++)
+          state[r, c] = lines[r][c] == '#' ? 1 : 0;
+
+      return state;
+    }
+
+    public static void Print(int[,] state)
+    {
+      int rows = state.GetLength(0);
+      int cols = state.GetLength(1);
+
+      using StreamWriter writer = new("output.txt");
+      for (int r = 0; r < rows; r++)
+      {
+        for (int c = 0; c < cols; c++)
+        {
+          writer.Write(state[r, c] == 1 ? '#' : '.');
+        }
+
+        writer.WriteLine();
+      }
+    }
+  }
+}
