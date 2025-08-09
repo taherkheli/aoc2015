74635d9bcaf404d288d016a53e420955b7dd6b1a
Bilal Taherkheli <bilal76@gmail.com>
2025-08-09 14:06:08 +0200

    - Day 23 completed

---
+++ src/D23/Instruction.cs
@@ -0,0 +1,13 @@
+﻿namespace aoc.D23
+{
+  public class Instruction(OpCode opCode = OpCode.Unknown, string op1 = "", string op2 = "")
+  {
+    private OpCode _opCode = opCode;
+    private string _op1 = op1;
+    private string _op2 = op2;
+
+    public OpCode OpCode { get => _opCode; set => _opCode = value; }
+    public string Op1 { get => _op1; set => _op1 = value; }
+    public string Op2 { get => _op2; set => _op2 = value; }
+  }
+}
---
+++ src/D23/Parser.cs
@@ -0,0 +1,42 @@
+﻿namespace aoc.D23
+{
+  public static class Parser
+  {
+    public static Instruction[] Parse(string path)
+    {
+      var lines = File.ReadAllLines(path);
+      var instructions = new Instruction[lines.Length + 1]; // i extra for appending exit instruction at the end
+
+      for (int i = 0; i < lines.Length; i++)
+        instructions[i] = ExtractInstruction(lines[i]);
+
+      instructions[^1] = new Instruction(OpCode.Exit);
+
+      return instructions;
+    }
+
+    private static Instruction ExtractInstruction(string line)
+    {
+      var instruction = new Instruction();
+      var s = line.Split(' ', StringSplitOptions.TrimEntries);
+
+      instruction.OpCode = (s[0]) switch
+      {
+        "hlf" => OpCode.Half,
+        "tpl" => OpCode.Triple,
+        "inc" => OpCode.Increment,
+        "jmp" => OpCode.Jump,
+        "jie" => OpCode.JumpIfEven,
+        "jio" => OpCode.JumpIfOne,
+        _ => OpCode.Unknown,
+      };
+
+      instruction.Op1 = s[1].Trim(',');
+
+      if (s.Length > 2)
+        instruction.Op2 = s[2];
+
+      return instruction;
+    }  
+  }
+}
